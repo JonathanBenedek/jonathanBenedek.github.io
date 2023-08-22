af36993b5907a61a45200f36c14b8fd52f060e9b
Jonathan Benedek <jonathan.benedek@is.com>
2023-08-22 22:38:40 +0300

    labelUpload inside listener

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,10 +2,10 @@ var CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dqtyj8zwp/upload';
 var CLONDINARY_UPLOAD_PRESET = 'wps8scvk';
 var imgPreview = document.getElementById('img-preview');
 var fileUpload = document.getElementById('file-upload');
-var labelUpload = document.getElementById('labelUpload');
 fileUpload.addEventListener('change', function(event) {
     var file = event.target.files[0];
     var formData = new FormData();
+    var labelUpload = document.getElementById('labelUpload');
     formData.append('file', file);
     formData.append('upload_preset', CLONDINARY_UPLOAD_PRESET);
     axios({
